9ed2fb9c5bb28603eaedf825987d270e573109bf
24384316+ikazuk@users.noreply.github.com <kazuki.ichinose@fixstars.com>
2026-07-03 01:31:44 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; config.android_sdk.accept_license = true; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.android-tools
+          ];
+        };
+      });
+}
