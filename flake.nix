ec802dc8ed59d956e99b7acab432466df8ad2d5e
24384316+ikazuk@users.noreply.github.com <kazuki.ichinose@fixstars.com>
2026-07-03 01:48:07 +0900

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,12 +7,39 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; config.android_sdk.accept_license = true; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+        };
+
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          platformVersions = [ "36" "35" "34" ];
+          buildToolsVersions = [ "36.0.0" "35.0.0" "34.0.0" ];
+          includeNDK = false;
+          includeEmulator = false;
+          includeSystemImages = false;
+        };
+
+        androidSdk = androidComposition.androidsdk;
       in {
         devShells.default = pkgs.mkShell {
-          packages = [
+          buildInputs = [
+            pkgs.jdk17
+            pkgs.gradle
             pkgs.android-tools
+            androidSdk
           ];
+
+          JAVA_HOME = "${pkgs.jdk17}";
+          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+
+          shellHook = ''
+            export PATH="${pkgs.jdk17}/bin:$PATH"
+          '';
         };
       });
 }
